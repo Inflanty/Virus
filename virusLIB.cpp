03571a2f376e86af0e54df1b71b9396630a31c29
Jan Glos <jan.glos@creadis.com>
2018-03-08 15:47:35 +0100

    Emptty user library added

---
+++ virusLIB.cpp
@@ -0,0 +1,8 @@
+/*
+virusLIB.cpp
+
+User library for VIRUS purpose
+Author  :: Jan Glos
+Date    :: 8/03/2018
+Place   :: Denmark, Aarhus
+*/
---
+++ virusLIB.h
@@ -0,0 +1,22 @@
+/*
+virusLIB.h
+
+User library for VIRUS purpose
+Author  :: Jan Glos
+Date    :: 8/03/2018
+Place   :: Denmark, Aarhus
+
+Class and Method definitions for virus app
+*/
+
+class virus {
+
+};
+
+class citizen {
+
+};
+
+class doctor {
+
+};
