ee1f41be4597da934ee9c58bce0eaa6314972c53
U-DIS\jan.glos <jan.glos@TPT048.dis.local>
2018-03-09 14:05:30 +0100

    Added docImg | README update

--- README.md
+++ README.md
@@ -3,3 +3,16 @@ Virus spread
 
 C++ program, showing virus spreading. Just little think right now.
 The point is OOP - try to use Object to run this task. 
+
+
+Virus Spread :
+* y = 1/4 * log(t) + 4 
+** t = [0 : 1]
+
+![](docImg/fnc.png)
+
+Virus clean :
+* y = t^2
+** t = [0 : 1]
+
+![](docImg/fnc_clean.png)
--- virusLIB.h
+++ virusLIB.h
@@ -8,15 +8,59 @@ Place   :: Denmark, Aarhus
 
 Class and Method definitions for virus app
 */
+#include <iostream>
+using namespace std;
 
 class virus {
 
+  float reproductionRate;    // rate of reproduction, in %
+  float resistance;          // resistance against drugs, in %
+  static const float defaultReproductionRate = 0.1;
+
+public:
+    Virus(float newResistance);
+    Virus(float newReproductionRate, float newResistance);
+    ~Virus();
+    Virus* reproduce(float immunity);
+    bool survive(float immunity);
 };
 
-class citizen {
+virus::Virus (float newResistance){
+   cout << "Object VIRUS is being created,\nResistance = " << newResistance << "\nReproduction rate default = "<< defaultReproductionRate << endl;
+   resistance = newResistance;
+   reproductionRate = dafaultReproductionRate;
+}
 
+virus::Virus (float newReproductionRate, float newResistance){
+   cout << "Object VIRUS is being created,\nResistance = " << newResistance << "\nReproduction rate = " << newReproductionRate << endl;
+   resistance = newResistance;
+   reproductionRate = newReproductionRate;
+}
+
+virus::~Virus (void){
+    cout << "Virus is beeing delted !" << endl;
+}
+
+bool virus::survive (float immunity){
+  //  live :: actual live [0 - 1]
+  if (immunity > live){
+
+  } else if (immunity < live){
+
+  } else if (immunity == live){
+
+  }
+}
+
+class citizen {
+  int age;
+  char gender;
+  char illness;
+  char symptoms;
 };
 
 class doctor {
-
+  int skills;
+  int expirience;
+  int availability;
 };
