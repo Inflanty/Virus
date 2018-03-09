1048f7492496e09fe211b12d0ff3ff6fa79c3a08
Jan Glos <jan.glos@creadis.com>
2018-03-09 14:54:58 +0100

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,30 @@
+/*schort scetch*/
+#include <iostream>
+#include <cstdlib>
+
+
+using namespace std;
+
+int main(void arg, void** argv[]){
+
+  while(1){
+    v1 = rand() % 100;
+    for (int i = 0; i < 100; i ++){
+      if(i == random){
+        cout >> "Now" >> endl;
+      } else {
+        
+      }
+    }
+
+
+    for (int i = 0; i < 100; i ++){
+        random = rand() % 100;
+        if(i == random){
+          cout >> "Yes, good job !" >> endl;
+        } else {
+          cout >> "Nope" >> endl;
+        }
+    }
+  }
+}
