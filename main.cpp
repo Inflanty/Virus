ac2329192ee17ef8eb1bfa5e55ee9047a01d4b7b
Jan Glos <jan.glos@creadis.com>
2018-03-12 16:12:33 +0100

    New fnc in main()

--- main.cpp
+++ main.cpp
@@ -1,30 +1,59 @@
 /*schort sketch*/
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 
+#include "virusLIB.h"
 
 using namespace std;
+int tmp = 0;
 
-int main(void arg, void** argv[]){
+void ux_randOne(){
+  int random = rand() % 100;
 
-  while(1){
-    v1 = rand() % 100;
-    for (int i = 0; i < 100; i ++){
-      if(i == random){
-        cout >> "Now" >> endl;
-      } else {
+  for (int i = 0; i < 100; i ++){
+    if(i == random){
+      cout << "Now\nMatches ! : " << i << endl;
+    } else {
 
-      }
     }
+  }
+}
 
-
+void uv_randMulti(){
+  while(1){
+    tmp ++;
     for (int i = 0; i < 100; i ++){
-        random = rand() % 100;
+        int random = rand() % 100;
         if(i == random){
-          cout >> "Yes, good job !" >> endl;
+          cout << "Yes, good job !\nNumber of iterations : " << tmp << endl;
+          tmp = 0;
         } else {
-          cout >> "Nope" >> endl;
+          cout << "Nope" << endl;
         }
     }
+  }
+}
+
+int main(){
+  srand( time( NULL ) );
+
+  //uv_randMulti();
+  virus virusOne(0.5);
+  float live = 0, def = 0.4;
+  cout << "Provide life level for test : " << endl;
+  cin >> live;
+
+  bool ans = virusOne.survive(live, def);
+  if(ans){
+    cout << "Virus didn't survive" << endl;
+    virusOne.~virus();
+    return 0
+  }else {
+    cout << "Virus survived" << endl;
+    return 0;
+  }
+  while(1){
+
   }
 }
--- virusLIB.h
+++ virusLIB.h
@@ -13,44 +13,44 @@ using namespace std;
 
 class virus {
 
-  float reproductionRate;    // rate of reproduction, in %
-  float resistance;          // resistance against drugs, in %
-  static const float defaultReproductionRate = 0.1;
+  float reproductionRate;    // rate of reproduction, in 0-1
+  float resistance;          // resistance against drugs, in 0-1
+  float defaultReproductionRate = 0.1;
 
 public:
-    Virus(float newResistance);
-    Virus(float newReproductionRate, float newResistance);
-    ~Virus();
-    Virus* reproduce(float immunity);
-    bool survive(float immunity);
+    virus(float newResistance);
+    virus(float newReproductionRate, float newResistance);
+    ~virus();
+
+    bool survive(float live, float immunity);
 };
 
-virus::Virus (float newResistance){
+virus::virus (float newResistance){
    cout << "Object VIRUS is being created,\nResistance = " << newResistance << "\nReproduction rate default = "<< defaultReproductionRate << endl;
    resistance = newResistance;
-   reproductionRate = dafaultReproductionRate;
-}
+   reproductionRate = defaultReproductionRate;
+};
 
-virus::Virus (float newReproductionRate, float newResistance){
+virus::virus (float newReproductionRate, float newResistance){
    cout << "Object VIRUS is being created,\nResistance = " << newResistance << "\nReproduction rate = " << newReproductionRate << endl;
    resistance = newResistance;
    reproductionRate = newReproductionRate;
-}
+};
 
-virus::~Virus (void){
+virus::~virus (){
     cout << "Virus is beeing delted !" << endl;
-}
+};
 
-bool virus::survive (float immunity){
+bool virus::survive (float live, float immunity){
   //  live :: actual live [0 - 1]
   if (immunity > live){
-
+    return true;
   } else if (immunity < live){
-
+    return false;
   } else if (immunity == live){
-
+    return true;
   }
-}
+};
 
 class citizen {
   int age;
